f2b8e5b8fae086bae4ef55c8405495ed14114d9d
Shuangz <likeshuangz@gmail.com>
2017-04-26 03:05:58 +0800

    增加down状态下的验证

--- src/GeetestServiceProvider.php
+++ src/GeetestServiceProvider.php
@@ -15,10 +15,16 @@ class GeetestServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->publishes([__DIR__.'/config.php' => config_path('geetest.php')]);
+        $this->publishes([
+            __DIR__.'/config.php' => config_path('geetest.php')
+        ]);
 
         //echo $this->app['config']['app']['name'];
-        Route::get($this->app['config']['geetest']['path'], $this->app['config']['geetest']['invoke']);
+        Route::get(
+            $this->app['config']['geetest']['path'],
+            $this->app['config']['geetest']['invoke']
+        );
+
         Validator::extend('geetest', [$this, 'extendValidate']);
     }
 
@@ -36,25 +42,36 @@ class GeetestServiceProvider extends ServiceProvider
 
     public function extendValidate($attribute, $value, $parameters, $validator)
     {
+
         $geetest    = $this->app->make('geetest');
         $request    = $this->app->make('request');
         $attributes = $validator->attributes();
         $username   = $parameters[0];
         $user_id    = $request->has($username) ? $request->input($username) : mt_rand(10000, 99999);
+        $user_id    = $request->has($gtserver) ? $request->input($gtserver) : mt_rand(10000, 99999);
         $data = [
             'user_id'     => sha1($user_id),
             'client_type' => config('geetest.client_type'),
             'ip_address'  => $request->ip()
         ];
 
-        $result = $geetest->success_validate(
-                     $attributes['geetest_challenge'],
-                     $attributes['geetest_validate'],
-                     $attributes['geetest_seccode'],
-                     $data
-                 );
+
+
+        if ($request->has($gtserver)) {
+            $result = $geetest->success_validate(
+                         $attributes['geetest_challenge'],
+                         $attributes['geetest_validate'],
+                         $attributes['geetest_seccode'],
+                         $data
+                     );
+        } else {
+            $result = $geetest->fail_validate(
+                         $attributes['geetest_challenge'],
+                         $attributes['geetest_validate'],
+                         $attributes['geetest_seccode']
+                     );
+        }
 
         return (boolean) $result;
-
     }
 }
